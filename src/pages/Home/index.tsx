fe315033c6390c1ee60e58454badb4a1a7657530
Lucas Alcântara <lucasalcantara18@gmail.com>
2020-12-01 17:03:51 -0300

    estrutura basica com menu e sidebar

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,17 +1,22 @@
 /* eslint-disable linebreak-style */
 import React from 'react';
-import { Grid, Button } from '@material-ui/core';
-import classNames from 'classnames';
-import { FaGraduationCap, FaChalkboardTeacher } from 'react-icons/fa';
+import { useRecoilState } from 'recoil';
+import clsx from 'clsx';
+import sideBarState from '../../recoil/atom';
 import useStyles from './styles';
 import useWindowDimensions from '../../utils/windowsDimension';
 
 const HomePage = () => {
   const classes = useStyles();
   const { height, width } = useWindowDimensions();
+  const [open, setOpen] = useRecoilState(sideBarState);
 
   return (
-    <div className={classes.root}>
+    <div
+      className={clsx(classes.content, {
+        [classes.contentShift]: open,
+      })}
+    >
       width: {width} ~ height: {height}
       <h2>Welcome to Armazenagem</h2>
     </div>
