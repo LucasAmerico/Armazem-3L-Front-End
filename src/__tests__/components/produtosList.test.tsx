c7b1d612b1c3ad3007f98a6ebd10e5f0634f1a12
Lucas Américo <lucas.martinsti@outlook.com>
2020-12-21 15:59:00 -0300

    #N/A teste(components): Configura testes em componentes de Produtos

--- src/__tests__/components/produtosList.test.tsx
+++ src/__tests__/components/produtosList.test.tsx
@@ -1,9 +1,41 @@
 /* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable no-undef */
-import { render, screen, waitForElement } from '@testing-library/react';
+import { Typography } from '@material-ui/core';
+import { ListSharp } from '@material-ui/icons';
+import {
+  render,
+  screen,
+  waitForElement,
+  getByTestId,
+} from '@testing-library/react';
 import React from 'react';
 import { RecoilRoot } from 'recoil';
+import Lista from '../../components/List/list';
 import ProdutosLista from '../../pages/Produtos/components/produtos-list';
+
+const produtos = [
+  {
+    id: 1,
+    nome: 'Playstation 5',
+    peso: 1.0,
+    preco: 1.0,
+    qtd: 100,
+  },
+  {
+    id: 2,
+    nome: 'Xbox 1',
+    peso: 1.0,
+    preco: 1.0,
+    qtd: 200,
+  },
+  {
+    id: 3,
+    nome: 'Nintendo',
+    peso: 1.0,
+    preco: 1.0,
+    qtd: 300,
+  },
+];
 
 describe('Tests for ProdutosLista component', () => {
   it('Renders Correctly', async () => {
@@ -25,7 +57,18 @@ describe('Tests for ProdutosLista component', () => {
         <ProdutosLista />
       </RecoilRoot>,
     );
+    const titulo = screen.getByTestId('title');
 
-    expect(screen.getByTestId('title')).toHaveTextContent('Produtos');
+    expect(titulo.textContent).toBe(' Produtos ');
+  });
+  it('Should present a list of products', async () => {
+    // renderizar o componente
+    const { queryByTestId } = render(
+      <RecoilRoot>
+        <ProdutosLista />
+      </RecoilRoot>,
+    );
+
+    const lista = screen.getByTestId('lista');
   });
 });
