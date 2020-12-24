5b85e33aa3e05ae1e6761ace5c610d44a8bb5e64
Lucas Simão <lucas.caixeta@dtidigital.com.br>
2020-12-24 17:02:41 -0300

    Testes cardUsers component

---
+++ src/__tests__/cardUser.test.tsx
@@ -0,0 +1,46 @@
+/* eslint-disable no-undef */
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { RecoilRoot } from 'recoil';
+import CardUsers from '../components/Cards/CardUsers';
+import usuarioEnum from '../utils/enum/usuarioEnum';
+import imageEnum from '../utils/enum/imageEnum';
+
+it('Should render component', async () => {
+  // renderizar o componente
+
+  const { container } = render(
+    <RecoilRoot>
+      <CardUsers
+        titulo="Administrativo"
+        tipoUsuario={usuarioEnum.ADMINISTRATIVO}
+        route="/cargas"
+      />
+    </RecoilRoot>,
+  );
+  const card = screen.getByLabelText('card') as HTMLInputElement;
+  expect(card).toBeTruthy();
+});
+
+it('Should render correct props', async () => {
+  // renderizar o componente
+
+  const { container } = render(
+    <RecoilRoot>
+      <CardUsers
+        titulo="Administrativo"
+        tipoUsuario={usuarioEnum.ADMINISTRATIVO}
+        route="/cargas"
+      />
+    </RecoilRoot>,
+  );
+  const img = screen.getByRole('img') as HTMLInputElement;
+  const imageSrcExpect = `http://localhost/${imageEnum(
+    usuarioEnum.ADMINISTRATIVO,
+  )}`;
+
+  expect(img).toHaveProperty('src', imageSrcExpect);
+
+  const title = screen.getByText(/Administrativo/i);
+  expect(title).toBeTruthy();
+});
