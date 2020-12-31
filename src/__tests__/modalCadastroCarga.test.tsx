cd2edda203a463c605f0adb6d2ba9c8374d54f6f
Lucas Simão <lucas.caixeta@dtidigital.com.br>
2020-12-31 16:25:10 -0300

    Teste modal cadastro

--- src/__tests__/modalCadastroCarga.test.tsx
+++ src/__tests__/modalCadastroCarga.test.tsx
@@ -4,40 +4,6 @@ import { fireEvent, screen, render } from '@testing-library/react';
 import { RecoilRoot } from 'recoil';
 import CadastroCarga from '../components/modal-cadastro-carga/CadastroCarga';
 
-const carga = {
-  id: 1,
-  endereco: '902 Milan Stream Apt. 421 - Baton Rouge, OR / 39834',
-  frete: 100.0,
-  motoristaId: 1,
-  produtos: [],
-  listaProdutos: [
-    {
-      quantidade: 0,
-      produto: {
-        id: 1,
-        nome: 'string',
-        peso: 0,
-        preco: 0,
-        qtd: 1,
-        qtdCarga: 1,
-      },
-      id: 1,
-      nome: 'string',
-      peso: 0,
-      preco: 0,
-      qtd: 0,
-      qtdCarga: 0,
-    },
-  ],
-};
-
-// jest.mock('../services/CargaService');
-jest.mock('../services/CargaService', () => {
-  return {
-    postCarga: jest.fn(),
-  };
-});
-
 describe('Tests for Modal-detalhes-carga component', () => {
   it('Should render the component correctly', async () => {
     const handleClose = jest.fn();
@@ -63,7 +29,7 @@ describe('Tests for Modal-detalhes-carga component', () => {
 
     expect(screen.getAllByText(/Selecionar Produtos/i)).toBeTruthy();
   });
-  it('Should closes modal when button close is clicked', async () => {
+  it('Should call close modal funcrion when button close is clicked', async () => {
     const handleClose = jest.fn();
 
     render(
@@ -75,41 +41,4 @@ describe('Tests for Modal-detalhes-carga component', () => {
     fireEvent.click(screen.getByText(/Fechar/i));
     expect(handleClose).toHaveBeenCalled();
   });
-  it('Should adds new carga when button confirm is clicked', async () => {
-    const handleClose = jest.fn();
-    const handleSave = jest.fn();
-
-    render(
-      <RecoilRoot>
-        <CadastroCarga modal onClose={handleClose} />
-      </RecoilRoot>,
-    );
-
-    fireEvent.click(screen.getByText(/Fechar/i));
-    expect(handleClose).toHaveBeenCalled();
-  });
-  // it('Should render data component correctly', async () => {
-  //   const handleClose = jest.fn();
-
-  //   const { queryByTestId } = render(
-  //     <RecoilRoot>
-  //       <CadastroCarga modal onClose={handleClose} />
-  //     </RecoilRoot>,
-  //   );
-
-  //   const component = queryByTestId('dados');
-  //   expect(component).toBeTruthy();
-  // });
-  // it('Should render the data list component correctly', async () => {
-  //   const handleClose = jest.fn();
-
-  //   const { queryByTestId } = render(
-  //     <RecoilRoot>
-  //       <CadastroCarga modal onClose={handleClose} />
-  //     </RecoilRoot>,
-  //   );
-
-  //   const component = queryByTestId('lista-dados');
-  //   expect(component).toBeTruthy();
-  // });
 });
